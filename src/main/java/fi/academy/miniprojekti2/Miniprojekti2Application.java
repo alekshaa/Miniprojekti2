9abcd91096ae4ad11eecb50fdf78c95d15045aec
aimipe <35955223+aimipe@users.noreply.github.com>
2018-03-13 11:10:54 +0200

    Eka kommit

---
+++ src/main/java/fi/academy/miniprojekti2/Miniprojekti2Application.java
@@ -0,0 +1,12 @@
+package fi.academy.miniprojekti2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniprojekti2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniprojekti2Application.class, args);
+	}
+}
